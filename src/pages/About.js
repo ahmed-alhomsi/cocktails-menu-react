f3553a7d86cb8ef28f1ba2874c63e7b15671b1d4
ahmed-alhomsi <ahmed.alhomsi.official@gmail.com>
2022-04-24 22:13:28 +0300

    a cocktail menu project hooked to external database and api, from john smilga 15 react projects on fcc.

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 section="section-title">about us</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente nobis nesciunt consequuntur eius explicabo assumenda distinctio! Sit dolorum aut ab, porro voluptate ea quasi commodi atque repellat, natus id perspiciatis laboriosam, officiis labore saepe deserunt aspernatur non? Maiores, voluptates adipisci!</p>
+    </section>
+  )
+}
+
+export default About
